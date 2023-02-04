b2e27a299da0ccc8f708e7518fdc3400501638af
AtesiT <atesit@mail.ru>
2023-02-04 21:22:11 +0300

    add bet on lines

--- theMiniProject.py
+++ theMiniProject.py
@@ -1,18 +1,19 @@
 # TheProjectOfGame
 MAX_LINES = 3
-
+MAX_BET = 100
+MIN_BET = 1
 
 
 # Ставка
 def deposit():
     while True:
-        amount = input('Ваша ставка? "Р" ')
+        amount = input('Сколько вносите в игру? $')
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
                 break
             else:
-                print('Ставка должна быть больше ноля')
+                print('Внос должен быть больше ноля')
         else:
             print('Введите число')
     return amount
@@ -32,9 +33,35 @@ def get_number_of_lines():
             print('Введите число')
     return lines
 
+def get_bet():
+    while True:
+        amount = input('Какую ставку ставить на каждую линию? $')
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f'Сумма должна быть от {MIN_BET} - {MAX_BET} ')
+        else:
+            print('Введите число')
+    return amount
+
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
 
+        if total_bet > balance:
+            print(f'У тебя недостаточно денег для ставки, ваш баланс ${balance}')
+        else:
+            break
+
+    print(f'Вы поставили ${bet} на {lines} линий. Общая ставка: ${total_bet}')
+
+
+
+    print(balance, lines)
 main()
